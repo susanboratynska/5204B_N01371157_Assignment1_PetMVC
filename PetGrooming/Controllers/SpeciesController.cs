964901578fd9a460a6b6aad50ec818fa99a3ee47
Susan Boratynska <susan.boratynska@gmail.com>
2020-01-22 21:27:33 -0500

    First Commit

---
+++ PetGrooming/Controllers/SpeciesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetGrooming.Controllers
+{
+    public class SpeciesController : Controller
+    {
+        // GET: Species
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //TODO: Each line should be a separate method in this class
+        // List
+        // Show
+        // Add
+        // [HttpPost] Add
+        // Update
+        // [HttpPost] Update
+        // (optional) delete
+        // [HttpPost] Delete
+    }
+}
